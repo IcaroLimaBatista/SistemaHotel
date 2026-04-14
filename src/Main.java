95ea5970fcb6d18269139e4f54307121ffa9ccd5
IcaroLimaBatista <icarolimabatista@gmail.com>
2026-04-14 12:03:18 -0300

    Atualizacao com sistema completo, cadastro, listagem, busca e ordenacao

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,37 @@ import Menus.HotelServices;
 import Menus.Input;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     static void main(String[] args) {
         Input input = new Input();
-        int cont = 0;
         int option;
-        HotelServices hotel = new HotelServices(cont);
+        Boolean end = false;
+        HotelServices hotel = new HotelServices();;
 
-        while (true) {
+        while (!end) {
             input.menu();
             option = input.option;
+
             switch (option) {
                 case 1:
                     hotel.cadastroReserva();
                     break;
                 case 2:
-                    System.out.println("Digite o nome do cliente: ");
+                    hotel.listarReservas();
                     break;
+                case 3:
+                    hotel.buscarReserva();
+                    break;
+                case 4:
+                    hotel.orderReservasDias();
+                    hotel.listarReservas();
+                    break;
+                case 5:
+                    end = true;
             }
 
-            break;
         }
 
     }
--- src/Menus/HotelServices.java
+++ src/Menus/HotelServices.java
@@ -6,60 +6,132 @@ public class HotelServices {
     String tipoQuarto;
     Integer numeroDias;
     Double valorDiaria;
-    Integer contC;
-    String[][] listarReservas= new String[3][9];
+    Double valorTotal;
+    Integer contC = -1;
+    public String[][] listarReservas= new String[4][10];
 
-    //Constructor void
-    public HotelServices(Integer cont) {
-        this.contC = cont;
-    }
 
     public void cadastroReserva(){
+        if (contC < listarReservas[0].length - 1){
+            Scanner sc = new Scanner(System.in);
+            contC++;
+            System.out.println("Digite o nome do cliente: ");
+            nomeCliente = sc.nextLine();
+            listarReservas[0][contC] = nomeCliente;
+
+            System.out.println("Digite o tipo de quarto: ");
+            while (true) {
+                System.out.println("Standard \n Premium \n The Lux");
+                String input = sc.nextLine();
+                input = input.toUpperCase();
+                if (input.equals("STANDARD") || input.equals("PREMIUM") || input.equals("THE LUX")) {
+                    tipoQuarto = input;
+                    listarReservas[1][contC] = tipoQuarto;
+                    break;
+                }else {
+                    System.out.println("Quarto inválido!");
+                }
+            }
+
+            System.out.println("Digite o número de dias: ");
+            while (true) {
+                if (!sc.hasNextInt()) {
+                    System.out.println("Número inválido!");
+                    sc.nextLine();
+                }else{
+                    numeroDias = sc.nextInt();
+                    listarReservas[2][contC] = numeroDias.toString();
+                    break;
+                }
+            }
+
+            System.out.println("Digite o valor da diária: ");
+            while (true) {
+                if (!sc.hasNextDouble()) {
+                    System.out.println("Valor inválido!");
+                    sc.nextLine();
+                }else {
+                    valorDiaria = sc.nextDouble();
+                    valorTotal = valorDiaria * numeroDias;
+                    listarReservas[3][contC] = valorTotal.toString();
+                    break;
+                }
+            }
+        }else {
+            System.out.println("Limite de reservas excedidos.");
+        }
+
+
+    }
+
+    public void listarReservas(){
+        for (int i = 0; i <= contC; i++) {
+            System.out.println("Reserva " + (i + 1));
+            System.out.println("Nome: " + listarReservas[0][i]);
+            System.out.println("Quarto: " + listarReservas[1][i]);
+            System.out.println("Dias: " + listarReservas[2][i]);
+            System.out.println("Valor: " + listarReservas[3][i]);
+            System.out.println("----------------------");
+        }
+    }
+
+    public void buscarReserva(){
         Scanner sc = new Scanner(System.in);
-        contC++;
+        String nomeReserva;
+        boolean encontrou = false;
 
-        System.out.println("Digite o nome do cliente: ");
-        nomeCliente = sc.nextLine();
-        listarReservas[contC][0] = nomeCliente;
+        System.out.print("Digite o nome do cliente: ");
+        nomeReserva = sc.nextLine();
 
-        System.out.println("Digite o tipo de quarto: ");
-        while (true) {
-            System.out.println("Standard \n Premium \n The Lux");
-            String input = sc.nextLine();
-            input = input.toUpperCase();
-            if (input.equals("STANDARD") || input.equals("PREMIUM") || input.equals("THE LUX")) {
-                tipoQuarto = input;
-                listarReservas[contC][1] = tipoQuarto;
-                break;
-            }else {
-                System.out.println("Quarto inválido!");
-            }
+        if(nomeReserva == null || nomeReserva.isEmpty()){
+            System.out.println("Nome incorreto!");
+            return;
         }
-        sc.nextLine();
 
-        System.out.println("Digite o número de dias: ");
-        while (true) {
-            if (!sc.hasNextInt()) {
-                System.out.println("Número inválido!");
-                sc.nextLine();
-            }else{
-                numeroDias = sc.nextInt();
-                listarReservas[contC][2] = numeroDias.toString() + " dias";
-                break;
+        nomeReserva = nomeReserva.toLowerCase();
+
+        for (int i = 0; i <= contC; i++) {
+            String nome = listarReservas[0][i];
+            if (nome != null && nome.toLowerCase().contains(nomeReserva)) {
+                System.out.println("Reserva encontrada:");
+                System.out.println("Nome: " + listarReservas[0][i]);
+                System.out.println("Quarto: " + listarReservas[1][i]);
+                System.out.println("Dias: " + listarReservas[2][i]);
+                System.out.println("Valor: " + listarReservas[3][i]);
+                System.out.println("----------------------");
+
+                encontrou = true;
             }
         }
 
-        System.out.println("Digite o valor da diária: ");
-        while (true) {
-            if (!sc.hasNextDouble()) {
-                System.out.println("Valor inválido!");
-                sc.nextLine();
-            }else {
-                valorDiaria = sc.nextDouble();
-                listarReservas[contC][3] = valorDiaria.toString();
-            }
+        if (!encontrou) {
+            System.out.println("Nenhuma reserva encontrada.");
         }
 
     }
 
+    public void orderReservasDias(){
+
+        for (int i = 0; i <= contC; i++) {
+            for (int j = 0; j < contC; j++) {
+
+                if (listarReservas[2][j] == null || listarReservas[2][j + 1] == null) {
+                    continue;
+                }
+
+                int dias1 = Integer.parseInt(listarReservas[2][j]);
+                int dias2 = Integer.parseInt(listarReservas[2][j + 1]);
+
+                if (dias1 < dias2) {
+                    for (int k = 0; k < listarReservas.length; k++) {
+                        String temp = listarReservas[k][j];
+                        listarReservas[k][j] = listarReservas[k][j + 1];
+                        listarReservas[k][j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Ordenado!");
+    }
 }
